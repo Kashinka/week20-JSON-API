799fd6a40ce569ca0584184759ab66dfe1f2d3e3
Kashinka <azalena@yandex.ru>
2022-12-03 14:32:19 +0300

    Added other functions

--- marvel.js
+++ marvel.js
@@ -1,3 +1,23 @@
 function infoSuperman() {
+
     document.getElementById('result').innerHTML = (`Инфо о супермэне`);
+
+}
+
+function infoSpiderman() {
+
+    document.getElementById('result').innerHTML = (`Инфо о человеке-пауке`);
+    
+}
+
+function infoBatman() {
+
+    document.getElementById('result').innerHTML = (`Инфо о бэтмэне`);
+    
+}
+
+function infoHulk() {
+
+    document.getElementById('result').innerHTML = (`Инфо о Халке`);
+    
 }
