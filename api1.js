1c50ebad8c5eb56ca275f6d9ff8765b4212eb531
Kashinka <azalena@yandex.ru>
2022-12-04 17:21:33 +0300

    Added function

---
+++ api1.js
@@ -0,0 +1,15 @@
+fetch('https://api.github.com/users/Kashinka/repos')
+    .then(response => response.json())
+
+            .then(data => {
+
+                console.log(data);
+                data.forEach(item => {
+
+                    document.getElementById('result').innerHTML += `<div>${item.name}</div>`
+
+                });
+
+            })
+            
+    .catch(err => console.log(err));
